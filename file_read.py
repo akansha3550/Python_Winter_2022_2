593c9372e0c33452f9d2e47a10bebe79fd4eea43
akans <akanshachaudhary999@gmail.com>
2022-11-24 16:27:15 +0100

    Read text file - line by line

--- file_read.py
+++ file_read.py
@@ -1,10 +1,20 @@
 my_file = 'hello.txt'
-f = open(my_file, 'r')
-lines = f.readlines()
-i= 0
-for line in lines:
-    i+=1
-    print(f'{1}: {line}', end='')
+# f = open(my_file, 'r')
+# lines = f.readlines()
+# i= 0
+# for line in lines:
+#     i+=1
+#     print(f'{1}: {line}', end='')
+# f.close()
+
+with open(my_file, 'r') as f:
+    lines = f.readlines()
+    i=0
+    for line in lines:
+        i+=1
+        print(f'{i}: {line}', end='')
 f.close()
+
+
 
 
