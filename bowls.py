1d96c4399eba968d979af74e2797ea4c82a041f3
akans <akanshachaudhary999@gmail.com>
2022-12-01 12:55:16 +0100

    Bowls using 4 approaches

--- bowls.py
+++ bowls.py
@@ -5,50 +5,43 @@
 #         * *
 #          *
 
-n = 6
+
 # sum the number of bowls given the number of rows(n)
+
+def sum_bowls_loop(rows):
+    i = 1
+    bowls = 0
+    while i < rows:
+        bowls += i
+        i += 1
+        if i == rows:
+            bowls += i
+            return bowls
+
+
+def sum_bowls_loop2(rows):
+    s = 0
+    for i in range(1, rows+1):
+        s += i
+    return s
+
+
 def sum_bowls_recursive(n):
-    if n == 1:
+    if n==1:
         return 1
     else:
         s = n + sum_bowls_recursive(n - 1)
         return s
 
 
-
-
-def sum_bowls_loop(n):
-    S = 0
-    for i in range(1, n + 1):
-        S = S + i
-    return S
-
-
-
-
 def sum_bowls_seq(n):
-    return int(((1+n)/2)*n)
+    return int(n*(n+1)/2)
 
 
-
-
-def time_func(func, n):
-    t0 = time()
-    print('Calling: {}'.format(func))
-    print(func(n))
-    t1 = time()
-    elapsed = t1 - t0
-    print('running {}, took: {}'.format(func, elapsed))
-
-
-
-#n = 998
-
-
-
-n = 20000000
-
-
-
-time_func(sum_bowls_loop, n)
-time_func(sum_bowls_seq, n)
+#n = 6
+#print('Sum bowls using loop: {} = {}'.format(n, sum_bowls_loop(n)))
+n = 7
+print('Sum bowls using loop: {} = {}'.format(n, sum_bowls_loop(n)))
+print('Sum bowls using loop2: {} = {}'.format(n, sum_bowls_loop2(n)))
+print('Sum bowls using recursion: {} = {}'.format(n, sum_bowls_recursive(n)))
+print('Sum bowls using sequence: {} = {}'.format(n, sum_bowls_seq(n)))
