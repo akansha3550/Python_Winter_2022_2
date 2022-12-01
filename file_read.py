c0d9e1a2cdbcf8ee8c7f8a91cff91e9fdfdf68b4
akans <akanshachaudhary999@gmail.com>
2022-12-01 19:33:54 +0100

    Read file in a text file

--- file_read.py
+++ file_read.py
@@ -9,9 +9,9 @@ my_file = 'hello.txt'
 
 with open(my_file, 'r') as f:
     lines = f.readlines()
-    i=0
+    i = 0
     for line in lines:
-        i+=1
+        i += 1
         print(f'{i}: {line}', end='')
 f.close()
 
