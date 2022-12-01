b6abde2d95c3e4ba21c2011c79b2dbd737aeadaf
akans <akanshachaudhary999@gmail.com>
2022-12-01 23:59:29 +0100

    Read a file that is relative to the location of my script

--- file_read_json.py
+++ file_read_json.py
@@ -1,8 +1,15 @@
 import json
+import os
 
+print('Current Working dir: {}' .format(os.getcwd()))
+print('My script: {}'.format(__file__))
+print('My Python folder: {}'.format(os.path.dirname(__file__)))
 my_out_file = 'C:/Users//akans/my_matrix_out.json'
 my_out_file = 'my_matrix_out.json'
-with open(my_out_file, 'r') as f:
+
+my_file = os.path.join(os.path.dirname(__file__),my_out_file)
+
+with open(my_file, 'r') as f:
     m = json.load(f)
     print(m)
     print(type(m[0][0]))
