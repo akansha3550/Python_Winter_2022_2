2f04792987039b85834426c43c9922da21f3dfe5
akans <akanshachaudhary999@gmail.com>
2022-12-13 19:58:40 +0100

    space occupied

--- space_occupied.py
+++ space_occupied.py
@@ -10,11 +10,11 @@ print('file count:', count)
 
 #computes the total space occupied by all those files.
 
-list_of_files = filter( os.path.isfile,
-                        glob.glob(dir_name + '/**/*', recursive=True) )
+list_of_files = filter(os.path.isfile,
+                        glob.glob(dir_name + '/**/*', recursive=True))
 
-files_with_size = [ (file_path, os.stat(file_path).st_size)
-                    for file_path in list_of_files ]
+files_with_size = [(file_path, os.stat(file_path).st_size)
+                    for file_path in list_of_files]
 
 for file_path, file_size in files_with_size:
     print("{} : {}MB".format(file_path, round(file_size / (1024 * 1024), 3)))
